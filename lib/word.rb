057339202b63a5c810b0df51be8ac424256a4fb0
gitKrystan <kmenne@gmail.com>
2016-01-15 08:43:30 -0800

    generate unique id for each Word

--- lib/word.rb
+++ lib/word.rb
@@ -1,7 +1,18 @@
 class Word
-  attr_reader(:name)
+  attr_reader(:name, :id)
+
+  @@last_id = 0
 
   def initialize(arguments)
     @name = arguments[:name]
+    @id = generate_id().to_s()
+  end
+
+  def generate_id
+    @@last_id += 1
+  end
+
+  def self.clear
+    @@last_id = 0
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,14 +3,32 @@ require('word')
 require('pry')
 
 describe(Word) do
+  before() do
+    Word.clear()
+  end
+
   describe('#initialize') do
     it('creates a new word object with an id') do
       test_word = a_new_word()
       expect(test_word.name()).to(eq('test'))
+      expect(test_word.id()).to(eq('1'))
+    end
+  end
+
+  describe('#generate_id') do
+    it('generates a unique id for each word') do
+      first_word = a_first_word()
+      test_word = a_new_word()
+      expect(first_word.id()).to(eq('1'))
+      expect(test_word.id()).to(eq('2'))
     end
   end
 end
 
 def a_new_word
   Word.new({:name => 'test'})
+end
+
+def a_first_word
+  Word.new({:name => 'first'})
 end
